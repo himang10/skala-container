e3bb66a5a03277186d444a2b889a267df7b668da
himang10 <yongwooyi@gmail.com>
2026-08-20 10:42:45 +0900

    update sample code & 00.sample-container

--- 03.sample-container/03.frontend/run-frontend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -d \
-  --name frontend \
-  --network skala \
-  -p 9090:80 \
-  frontend:1.0
--- 03.sample-container/04.vue-frontend/run-frontend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -d \
-  --name vue-frontend \
-  --network skala \
-  -p 9090:80 \
-  vue-frontend:1.0
